b608bfdc86e25df23590d413977a50d01f70cb84
Arthur LORENT <arthur@acid.fr>
2020-12-03 11:37:16 +0100

    Fixed webpack.mix.js file

--- webpack.mix.js
+++ webpack.mix.js
@@ -22,34 +22,34 @@ mix
     .copyDirectoryOutsideMixWorkflow('node_modules/@fortawesome/fontawesome-free/webfonts', 'public/fonts/fontawesome')
     .copyDirectoryOutsideMixWorkflow('resources/images', 'public/images')
 
-    // js **************************************************************************************************************
-    // admin
+    // JS **************************************************************************************************************
+    // Admin
     .js('resources/js/templates/admin/brickables/carousel/edit.js', 'public/js/templates/admin/brickables/carousel')
     .js('resources/js/templates/admin/library-media/index.js', 'public/js/templates/admin/library-media')
     .js('resources/js/templates/admin/library-media/edit.js', 'public/js/templates/admin/library-media')
-    // front
+    // Front
     //
-    // brickables
+    // Brickables
     .js('resources/js/brickables/carousel.js', 'public/js/brickables')
-    // base
+    // Base
     .js('resources/js/global/admin.js', 'public/js/admin.js')
     .scripts(['vendor/realrashid/sweet-alert/resources/js/sweetalert.all.js', 'public/js/admin.js'], 'public/js/admin.js')
     .js('resources/js/global/front.js', 'public/js/front.js')
     .scripts(['vendor/realrashid/sweet-alert/resources/js/sweetalert.all.js', 'public/js/front.js'], 'public/js/front.js')
 
-    // sass ************************************************************************************************************
-    // admin
+    // SASS ************************************************************************************************************
+    // Admin
     //
-    // front
+    // Front
     .sass('resources/sass/templates/front/news/page/show.scss', 'public/css/templates/front/news/page')
     .sass('resources/sass/templates/front/contact/page/show.scss', 'public/css/templates/front/contact/page')
-    // brickables
+    // Brickables
     .sass('resources/sass/brickables/carousel.scss', 'public/css/brickables')
-    // base
+    // Base
     .sass('resources/sass/global/_admin.scss', 'public/css/admin.css')
     .sass('resources/sass/global/_front.scss', 'public/css/front.css')
 
-    // config **********************************************************************************************************
+    // Config **********************************************************************************************************
     .webpackConfig({
         module: {
             rules: [
