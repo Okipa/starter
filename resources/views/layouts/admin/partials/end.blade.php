72a4470b4f42b25363a1e4b37ce4642ece0ca174
Arthur LORENT <arthur@acid.fr>
2020-12-03 12:31:21 +0100

    WIP

--- resources/views/layouts/admin/partials/end.blade.php
+++ resources/views/layouts/admin/partials/end.blade.php
@@ -1,7 +1,7 @@
 @include('layouts.common.partials.sweetalert')
-<script src="{{ mix('/js/manifest.js') }}"></script>
-<script src="{{ mix('/js/vendor.js') }}"></script>
-<script src="{{ mix('/js/admin.js') }}"></script>
+<script src="{{ mix('/js/manifest.js') }}" defer></script>
+<script src="{{ mix('/js/vendor.js') }}" defer></script>
+<script src="{{ mix('/js/admin.js') }}" defer></script>
 @brickablesJs
-@isset($js)<script src="{{ $js }}"></script>@endisset
+@isset($js)<script src="{{ $js }}" defer></script>@endisset
 @stack('scripts')
