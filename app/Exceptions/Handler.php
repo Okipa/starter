de21c14502af1e60dfe977b5cfc5dc08d04a56b5
Arthur LORENT <arthur@acid.fr>
2020-12-24 15:08:13 +0100

    Replaced Vessel by Laravel Sail

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -5,6 +5,7 @@ namespace App\Exceptions;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Illuminate\Validation\ValidationException;
 use Sentry\State\Scope;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Throwable;
 
@@ -27,17 +28,17 @@ class Handler extends ExceptionHandler
     protected $dontFlash = ['password', 'password_confirmation'];
 
     /**
-     * @param \Throwable $exception
+     * @param \Throwable $e
      *
      * @throws \Throwable
      */
-    public function report(Throwable $exception): void
+    public function report(Throwable $e): void
     {
-        if ($this->shouldReport($exception) && app()->bound('sentry')) {
-            app('sentry')->captureException($exception);
+        if ($this->shouldReport($e) && app()->bound('sentry')) {
+            app('sentry')->captureException($e);
         }
 
-        parent::report($exception);
+        parent::report($e);
     }
 
     /**
@@ -47,47 +48,49 @@ class Handler extends ExceptionHandler
      */
     public function register(): void
     {
-        //
+        $this->reportable(static function (Throwable $e) {
+            //
+        });
     }
 
     /**
      * @param \Illuminate\Http\Request $request
-     * @param \Throwable $exception
+     * @param \Throwable $e
      *
      * @return \Symfony\Component\HttpFoundation\Response
      * @throws \Throwable
      */
-    public function render($request, Throwable $exception): \Symfony\Component\HttpFoundation\Response
+    public function render($request, Throwable $e): Response
     {
         // Convert all non-http exceptions to a proper 500 http exception
         // if we don't do this exceptions are shown as a default template
         // instead of our own view in resources/views/errors/500.blade.php
         if (
             ! $request->expectsJson()
-            && $this->shouldReport($exception)
-            && ! $this->isHttpException($exception)
+            && $this->shouldReport($e)
+            && ! $this->isHttpException($e)
             && ! config('app.debug')
         ) {
-            $exception = new HttpException(500, __('An unexpected error has occurred.'));
+            $e = new HttpException(500, __('An unexpected error has occurred.'));
         }
 
-        return parent::render($request, $exception);
+        return parent::render($request, $e);
     }
 
     /**
      * Convert a validation exception into a response.
      *
      * @param \Illuminate\Http\Request $request
-     * @param \Illuminate\Validation\ValidationException $exception
+     * @param \Illuminate\Validation\ValidationException $e
      *
      * @return \Illuminate\Http\Response
      */
-    protected function invalid($request, ValidationException $exception)
+    protected function invalid($request, ValidationException $e)
     {
         if (! $request->expectsJson()) {
             toast(__('notify.invalid'), 'error');
         }
 
-        return parent::invalid($request, $exception);
+        return parent::invalid($request, $e);
     }
 }
