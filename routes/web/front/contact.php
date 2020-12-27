88fbb94868af4dc5feedd53f264ad311c948988f
Artu1000 <arthur.lorent@gmail.com>
2020-12-27 22:53:54 +0100

    Updated lightbox

--- routes/web/front/contact.php
+++ routes/web/front/contact.php
@@ -2,8 +2,13 @@
 
 use App\Http\Controllers\Front\ContactPageController;
 use Illuminate\Support\Facades\Route;
+use Spatie\Honeypot\ProtectAgainstSpam;
 
 Route::get(
     '/contact',
     [ContactPageController::class, 'show']
 )->name('contact.page.show');
+Route::post(
+    '/contact/message/send',
+    [ContactPageController::class, 'sendMessage']
+)->name('contact.sendMessage')->middleware(ProtectAgainstSpam::class);
