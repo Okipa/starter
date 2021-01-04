4c73f2ea554e8ffdd57eb0d05aa6c87561cdc21d
Arthur LORENT <arthur@acid.fr>
2021-01-04 17:22:12 +0100

    WIP

--- resources/js/vendor/lightbox.js
+++ resources/js/vendor/lightbox.js
@@ -3,7 +3,7 @@ _.each(document.querySelectorAll('[data-lightbox]'), (item) => {
     item.addEventListener('click', function (e) {
         e.preventDefault();
         const lightbox = GLightbox({
-            elements: [{href: this.getAttribute('href'), title: this.getAttribute('title')}],
+            elements: [{href: this.getAttribute('href')}],
             zoomable: false
         });
         lightbox.open();
